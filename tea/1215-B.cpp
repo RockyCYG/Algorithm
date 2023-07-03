095df5f5066c398dd237addf65b816c9977ce87f
RockyCYG <12011625@mail.sustech.edu.cn>
2023-07-03 18:32:17 +0800

    7.3

--- tea/1215-B.cpp
+++ tea/1215-B.cpp
@@ -9,15 +9,16 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+using i64 = long long;
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     int n;
     cin >> n;
-    int cnt[2] = {1, 0};
+    i64 cnt[2] = {1, 0};
     int s = 0;
-    int ans0 = 0, ans1 = 0;
+    i64 ans0 = 0, ans1 = 0;
     for (int i = 0; i < n; i++) {
         int x;
         cin >> x;
